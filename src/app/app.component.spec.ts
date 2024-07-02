9a0432385a5507ca903863f02bb014f16544e7e3
EduardoOrtegaZerpa <eduortza@gmail.com>
2024-07-02 19:30:48 +0100

    Inicial commit

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'scheduleHandler' title`, () => {
+  it(`should have the 'scheduleManager' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('scheduleHandler');
+    expect(app.title).toEqual('scheduleManager');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, scheduleHandler');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, scheduleManager');
   });
 });
